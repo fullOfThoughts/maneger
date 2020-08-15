df7225d6313c0346f2e37b05e81d420eccb1cf55
满脑的思绪呀 <15356636569@163.com>
2020-08-15 22:09:30 +0800

    first-commit

---
+++ src/containers/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button } from 'antd'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'antd/dist/antd.css'
+
+export default class App extends React.Component {
+  state = {}
+  render() {
+    return (
+      <div>
+        123
+        <Button>123</Button>
+      </div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './containers/app.jsx'
+ReactDOM.render(<App />, document.querySelector('#root'))
