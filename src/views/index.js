39252dde1639d36ad0e4050d2e9f2f5261ceb0d8
满脑的思绪呀 <15356636569@163.com>
2020-08-16 20:35:47 +0800

    对项目进行基于路由的懒加载

--- src/views/index.js
+++ src/views/index.js
@@ -1,8 +1,30 @@
-import Dashboard from './Dashboard'
-import Login from './Login'
-import Notfound from './Notfound'
-import Settings from './Settings'
-import Articlelist from './Article'
-import Articleedit from './Article/Edit'
+import Loadable from 'react-loadable'
+
+import { Loading } from '../components'
+
+const Dashboard = Loadable({
+  loader: () => import('./Dashboard'),
+  loading: Loading,
+})
+const Login = Loadable({
+  loader: () => import('./Login'),
+  loading: Loading,
+})
+const Notfound = Loadable({
+  loader: () => import('./Notfound'),
+  loading: Loading,
+})
+const Settings = Loadable({
+  loader: () => import('./Settings'),
+  loading: Loading,
+})
+const Articlelist = Loadable({
+  loader: () => import('./Article'),
+  loading: Loading,
+})
+const Articleedit = Loadable({
+  loader: () => import('./Article/Edit'),
+  loading: Loading,
+})
 
 export { Dashboard, Login, Notfound, Settings, Articlelist, Articleedit }
