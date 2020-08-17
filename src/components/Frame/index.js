96236b637eee0da59dd12f3b90a08cf8e3241e62
满脑的思绪呀 <15356636569@163.com>
2020-08-17 11:50:00 +0800

    完成文章列表

--- src/components/Frame/index.js
+++ src/components/Frame/index.js
@@ -4,8 +4,9 @@ import { withRouter } from 'react-router-dom'
 import { adminRoutes } from '../../routes'
 
 import Logo from './logo.png'
-import { Layout, Menu } from 'antd'
-const { Header, Content, Footer, Sider } = Layout
+import { Layout, Menu, ConfigProvider } from 'antd'
+import zhCN from 'antd/es/locale/zh_CN'
+const { Header, Content, Sider } = Layout
 
 @withRouter
 class Frame extends React.Component {
@@ -15,53 +16,52 @@ class Frame extends React.Component {
   }
   render() {
     return (
-      <Layout style={{ height: '100%' }}>
-        <Header
-          className="header"
-          style={{ background: 'rgba(0, 0, 0, 0.65)' }}
-        >
-          <div className="logo">
-            <img src={Logo} alt="123" width="180px" height="60px" />
-          </div>
-        </Header>
-        <Content style={{ padding: '0 50px', height: '100%' }}>
-          <Layout
-            className="site-layout-background"
-            style={{ padding: '24px 0', height: '100%' }}
+      <ConfigProvider locale={zhCN}>
+        <Layout style={{ height: '100%' }}>
+          <Header
+            className="header"
+            style={{ background: 'rgba(0, 0, 0, 0.65)' }}
           >
-            <Sider className="site-layout-background" width={200}>
-              <Menu
-                mode="inline"
-                selectedKeys={[this.props.location.pathname]}
-                style={{ height: '100%' }}
-                onClick={({ key }) => this.menu({ key })}
-              >
-                {adminRoutes.map((item, i) => {
-                  if (!item.title) {
-                    return null
-                  }
-                  return (
-                    <Menu.Item key={item.pathname} icon={<item.icon />}>
-                      {item.title}
-                    </Menu.Item>
-                  )
-                })}
-              </Menu>
-            </Sider>
-            <Content
-              style={{
-                paddingLeft: '16px',
-                minHeight: '100%',
-              }}
+            <div className="logo">
+              <img src={Logo} alt="123" width="180px" height="60px" />
+            </div>
+          </Header>
+          <Content style={{ padding: '0 50px', height: '100%' }}>
+            <Layout
+              className="site-layout-background"
+              style={{ padding: '24px 0', height: '100%' }}
             >
-              {this.props.children}
-            </Content>
-          </Layout>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©2018 Created by Ant UED
-        </Footer>
-      </Layout>
+              <Sider className="site-layout-background" width={200}>
+                <Menu
+                  mode="inline"
+                  selectedKeys={[this.props.location.pathname]}
+                  style={{ height: '100%' }}
+                  onClick={({ key }) => this.menu({ key })}
+                >
+                  {adminRoutes.map((item, i) => {
+                    if (!item.title) {
+                      return null
+                    }
+                    return (
+                      <Menu.Item key={item.pathname} icon={<item.icon />}>
+                        {item.title}
+                      </Menu.Item>
+                    )
+                  })}
+                </Menu>
+              </Sider>
+              <Content
+                style={{
+                  paddingLeft: '16px',
+                  minHeight: '100%',
+                }}
+              >
+                {this.props.children}
+              </Content>
+            </Layout>
+          </Content>
+        </Layout>
+      </ConfigProvider>
     )
   }
 }
--- src/views/Article/index.js
+++ src/views/Article/index.js
@@ -1,7 +1,64 @@
 import React from 'react'
+import { Card, Button, Table } from 'antd'
 export default class Articlelist extends React.Component {
   state = {}
   render() {
-    return <div>文章列表</div>
+    const dataSource = [
+      {
+        key: '1',
+        name: '胡彦斌',
+        age: 32,
+        address: '西湖区湖底公园1号',
+      },
+      {
+        key: '2',
+        name: '胡彦祖',
+        age: 42,
+        address: '西湖区湖底公园1号',
+      },
+    ]
+
+    const columns = [
+      //  columns 是对我们列的管理，比如这一列显示什么数据，这一列什么
+      {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '年龄',
+        dataIndex: 'age',
+        key: 'age',
+      },
+      {
+        title: '住址',
+        dataIndex: 'address',
+        key: 'address',
+      },
+      {
+        title: '操作',
+        dataIndex: 'action',
+        key: 'action',
+        render: (text, record, index) => {
+          console.log(text, record, index)
+          return <Button>编辑</Button>
+        },
+      },
+    ]
+    return (
+      <Card
+        title="文章列表"
+        bordered={false}
+        extra={<Button>导出文章</Button>}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <Table
+          dataSource={dataSource}
+          columns={columns}
+          pagination={{ hideOnSinglePage: true }}
+        />
+        ;
+      </Card>
+    )
   }
 }
