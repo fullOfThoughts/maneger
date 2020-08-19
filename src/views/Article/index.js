0cb2b7a9221d70cb64ab057b16eaee4c6f33f3f0
满脑的思绪呀 <15356636569@163.com>
2020-08-19 23:00:24 +0800

    进一步优化文章管理，完成了显示和文件导出

--- src/views/Article/index.js
+++ src/views/Article/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import XLSX from 'xlsx'
 import { getArticle } from '../../requests'
 import { Card, Button, Table, Tag } from 'antd'
-var moment = require('moment')
+const moment = require('moment')
+
 const columnMap = {
   //  定义一些映射关系
   title: '标题',
@@ -11,6 +13,7 @@ const columnMap = {
   id: 'id',
   action: '编辑',
   loading: false,
+  total: 0,
 }
 
 export default class Articlelist extends React.Component {
@@ -19,13 +22,20 @@ export default class Articlelist extends React.Component {
     total: 0,
     columns: [],
   }
+  //  初始化获取数据
   componentDidMount() {
+    this.getData()
+  }
+  delete = () => {
+    console.log()
+  }
+  //  获取数据
+  getData = () => {
     this.setState({ loading: true })
     getArticle().then((res) => {
       this.setState({ dataSource: res.data.list, total: res.data.total })
       const columnkey = Object.keys(res.data.list[0])
       columnkey[columnkey.length] = 'action'
-      console.log(columnkey)
       const columns = columnkey.map((item) => {
         //  直接抽离出对象的属性，在加工成列
         if (item === 'amount') {
@@ -53,11 +63,20 @@ export default class Articlelist extends React.Component {
           return {
             title: columnMap[item],
             key: item,
-            render: () => {
+            render: (text, record, index) => {
               return (
                 <>
-                  <Tag color="cyan">编辑</Tag>
-                  <Tag color="red">删除</Tag>
+                  <Button type="primary" success="true" size="small">
+                    编辑
+                  </Button>
+                  <Button
+                    type="primary"
+                    danger
+                    size="small"
+                    onClick={() => this.delete()}
+                  >
+                    删除
+                  </Button>
                 </>
               )
             },
@@ -75,12 +94,36 @@ export default class Articlelist extends React.Component {
       })
     })
   }
+  //  页面变化会触发
+  changePage = () => {
+    //  设置页数变化时更新内容
+    this.getData()
+  }
+  //  导出文件
+  exportFile() {
+    const data = this.state.dataSource
+    const head = Object.keys(data[0])
+
+    const contents = data.map((item) => {
+      item = Object.assign({}, item, {
+        createAt: moment(item.createAt).format('L'), //  将时间戳转换为时间在存进去
+      })
+      return Object.values(item)
+    })
+    // console.log(head, ...contents)
+    // /* convert state to workbook */
+    const ws = XLSX.utils.aoa_to_sheet([head, ...contents])
+    const wb = XLSX.utils.book_new()
+    XLSX.utils.book_append_sheet(wb, ws, 'SheetJS')
+    // // /* generate XLSX file and send to client */
+    XLSX.writeFile(wb, `sheetjs-${moment().format('L')}.xlsx`)
+  }
   render() {
     return (
       <Card
         title="文章列表"
         bordered={false}
-        extra={<Button>导出文章</Button>}
+        extra={<Button onClick={() => this.exportFile()}>导出文章</Button>}
         style={{ width: '100%', height: '100%' }}
       >
         <Table
@@ -88,8 +131,13 @@ export default class Articlelist extends React.Component {
           rowKey={(record) => record.id}
           dataSource={this.state.dataSource}
           columns={this.state.columns}
-          // pagination={{ hideOnSinglePage: true }}
-          pagination={{ pageSize: 5, total: this.state.total }}
+          pagination={{
+            pageSize: 5,
+            total: this.state.total,
+            showSizeChanger: true,
+            showQuickJumper: true,
+          }}
+          onChange={() => this.changePage()}
         />
       </Card>
     )
